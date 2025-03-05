b5e7f501a38b2d4827001c11302886e82945ba61
xivkals <ffxiv.kals@gmail.com>
2025-03-05 23:59:14 +0900

    fix...

--- lottery.js
+++ lottery.js
@@ -117,7 +117,7 @@ function CreateCombinations(progress=0) {
     // CreateCombinationsTH
     for (let t1 = 0; t1 < entry[0].length; t1++) {
         console.log('loop: t1, ', t1);
-        let temp = new Combination([...temp.selected], temp.combination.map(arr => [...arr]), temp.count);
+        let temp = new Combination(selected);
         temp.InsertM(0, entry[0][t1]);
 
         for (let t2 = t1 + 1; t2 < entry[0].length; t2++) {
